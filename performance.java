4e193db6680237e39c3428bb4f5cd681719328d8
ejmurray <ejmurray247@hotmail.com>
2013-05-03 12:53:55 +0200

    Update performance.java

--- performance.java
+++ performance.java
@@ -1,8 +1,8 @@
-   int truePositives = c_TruePosi;
-   int falsePositives = c_FalsePos;
-   int trueNegatives = c_TrueNega;
-   int falseNegatives = c_FalseNeg;
-   int total = truePositives + falsePositives + trueNegatives + falseNegatives; 
+int truePositives = c_TruePosi;
+int falsePositives = c_FalsePos;
+int trueNegatives = c_TrueNega;
+int falseNegatives = c_FalseNeg;
+int total = truePositives + falsePositives + trueNegatives + falseNegatives; 
 
 
 double accuracy = ((double)truePositives + trueNegatives) /total;
@@ -12,9 +12,10 @@ double balancedAccuracy = (specificity + sensitivity)/2;
 
 double top = (((double)truePositives * trueNegatives) - (falsePositives * falseNegatives));
 double bottom = Math.sqrt(((double)truePositives + falsePositives) * (truePositives + falseNegatives) * (trueNegatives + falsePositives) * (trueNegatives + falseNegatives));
-
 double mcc = top / bottom;
+
 double precision = (double)truePositives / (truePositives + falsePositives);
+double falsePositiveRate = (((double)falsePositives) / (falsePositives + trueNegatives));
 
 out_Accura = accuracy;
 out_Specif = specificity;
@@ -22,3 +23,4 @@ out_Sensit = sensitivity;
 out_Balanc = balancedAccuracy;
 out_MCC = mcc;
 out_Precis = precision;
+out_FalseP = falsePositiveRate;
